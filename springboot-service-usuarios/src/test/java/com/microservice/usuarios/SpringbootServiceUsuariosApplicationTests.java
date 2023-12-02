21ead192a6ac0c0a49b449d1f3bc3649614d06c8
Miguel Angel Vazquez Diaz <angel.vazquezia@uanl.edu.mx>
2023-12-02 02:52:34 -0600

    Creando Servicio Usuarios- Generando Entidades Usuario y Role- Relacion Many to Many

---
+++ springboot-service-usuarios/src/test/java/com/microservice/usuarios/SpringbootServiceUsuariosApplicationTests.java
@@ -0,0 +1,8 @@
+package com.microservice.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceUsuariosApplicationTests {
+}
